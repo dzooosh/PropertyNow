0f66014d9c4d3f38aba955551ddc526f8c01b085
SifisoSithole <103381999+SifisoSithole@users.noreply.github.com>
2023-06-15 19:04:56 +0000

    Commit message for adding the views for Property objects:
    Added views for Property objects that handle all default REST API actions.
    The views are implemented using Flask and are located in the property_views module.
    The views include a route for retrieving a list of properties from the database and another route for retrieving a specific property by its ID.
    The views utilize the Property class to interact with the underlying storage and retrieve the necessary data.
    Error handling is included to return appropriate responses in case of failures or missing properties.

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ from flask_mail import Mail, Message
 from itsdangerous import URLSafeTimedSerializer
 from werkzeug.security import generate_password_hash, check_password_hash
 from auth import Auth
+from views import property_views
 from models.user import User
 from models.property import Property
 
@@ -28,6 +29,7 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'your_username'
 app.config['MAIL_PASSWORD'] = 'your_password'
+app.register_blueprint(property_views, url_prefix="/property")
 
 AUTH = Auth()
 mail = Mail(app)  # handles mailing token to the user
