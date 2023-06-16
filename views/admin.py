17214013a465bc4089eb96666df5b9c798caf397
Alien <alongejoshua@gmail.com>
2023-06-16 17:18:00 +0100

    updates admin views for admin routes

--- views/admin.py
+++ views/admin.py
@@ -1,4 +1,8 @@
 """
 a view for `admin` that handles all admins REST API
 actions
+Admin - Add (user, admin, properties)
+        Remove (user, properties)
+        Update(properties, User (change user to admin))
+        Delete(Any)
 """
