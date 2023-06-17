fbf67cffa3dc96da4414b06c000a6ed156872c3c
SifisoSithole <sitholesifisobrian@gmail.com>
2023-06-17 13:00:27 +0200

    Implement /properties/images/<string:filename> endpoint and add test images
    Added the /properties/images/<string:filename> endpoint to retrieve images associated with properties.
    The endpoint allows clients to request images by filename and returns the corresponding image file.
    Also, included a directory named "images" containing test images that can be used to validate the web application functionality.
    The test images are compatible with the add_properties.py script for testing purposes.

--- add_properties.py
+++ add_properties.py
@@ -9,13 +9,13 @@ propertyClass = Property()
 def store_property_image(id):
     image_name = random.randint(1, 46)
     image_directory = './images'
-    output_directory = os.path.expanduser('~/property_images')
+    output_directory = os.path.expanduser('./property_images')
     os.makedirs(output_directory, exist_ok=True)
     image_path = os.path.join(image_directory, f'{image_name}.png')
     new_image_path = os.path.join(output_directory, f"{id}{os.path.splitext(f'{image_name}.png')[1]}")
     if os.path.exists(image_path):
         shutil.copy2(image_path, new_image_path)
-        print(f"Image '{image_filename}' copied and renamed to '{os.path.basename(new_image_path)}'.")
+        print(f"Image '{f'{image_name}.png'}' copied and renamed to '{os.path.basename(new_image_path)}'.")
 
 with open('properties.json', 'r') as file:
     data = json.load(file)
