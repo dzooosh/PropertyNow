dab7061077a3791555187e9c3e72dbe1fa0d6d63
Alien <alongejoshua@gmail.com>
2023-06-19 17:55:32 +0100

    update on user profile

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import secrets
 import json
 
 from flask import Flask, jsonify, request
-from flask_jwt_extended import JWTManager
+from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity
 from flask_mail import Mail, Message
-from auth.auth import Auth
+from auth import auth
 from views import property_views, admin, auth_views
 from models.user import User
 from flask_cors import CORS
@@ -26,7 +26,7 @@ app.register_blueprint(property_views, url_prefix="/properties")
 app.register_blueprint(admin, url_prefix="/admin")
 app.register_blueprint(auth_views, url_prefix="/auth")
 
-AUTH = Auth()
+AUTH = auth.Auth()
 mail = Mail(app)  # handles mailing token to the user
 
 
@@ -59,6 +59,52 @@ def forgot_password():
         return jsonify(js_msg)
     else:
         return jsonify({"error": "The email does not exist"}), 404
+
+@app.route('/user/profile', methods=['GET', 'POST'], strict_slashes=False)
+@jwt_required()
+def profile():
+    # Retrieve the user from the database
+    current_user = get_jwt_identity()
+    user = userClass.get_user(current_user['email'])
+
+    if request.method == 'POST':
+        # Retrieve the submitted form data
+        new_firstname = request.form.get('first_name')
+        new_lastname = request.form.get('last_name')
+        new_email = request.form.get('email')
+
+        if user:
+            # Update the user data with the submitted changes
+            updated_data = {}
+            if new_firstname:
+                updated_data['first_name'] = new_firstname
+            if new_lastname:
+                updated_data['last_name'] = new_lastname
+            if new_email:
+                updated_data['email'] = new_email
+            
+            expected_fields = ['email', 'first_name', 'last_name']
+
+            # check if no other field is being changed
+            for field in request.form:
+                if field not in expected_fields:
+                    return jsonify({"error": 
+                                    "Invalid field: {}".format(field)}), 403
+
+            # Update the user in the database
+            if userClass.update_user(user['_id'], updated_data):
+                return jsonify({'success': 'Profile updated successfully.'})
+            else:
+                return jsonify({"error": "Failed to update profile. Please try again."})
+        else:
+            return jsonify({'error': 'User not found.'}), 404
+
+    # if the request is `GET`
+    if user:
+        user['_id'] = str(user['_id'])
+        return jsonify(user), 200
+    else:
+        return jsonify({'error':'No such User'}), 404
 
 
 if __name__ == '__main__':
